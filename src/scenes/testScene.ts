f101b2b62c9a2159369810cab772edca8ff14037
Alexis-NM <alexis.na.me@gmail.com>
2024-04-25 19:28:09 +0200

    exercice 3

--- src/scenes/testScene.ts
+++ src/scenes/testScene.ts
@@ -1,6 +1,6 @@
 /*
- * STAIRCASE SWITCHES
- */
+* STACK GEN AND SORT
+*/
 
 import { Engine } from "@babylonjs/core/Engines/engine";
 import { Scene } from "@babylonjs/core/scene";
@@ -10,23 +10,9 @@ import { StandardMaterial } from "@babylonjs/core/Materials/standardMaterial";
 import { CreateSceneClass } from "../createScene";
 import { DirectionalLight } from "@babylonjs/core/Lights/directionalLight";
 import { HemisphericLight } from "@babylonjs/core/Lights/hemisphericLight";
-import { PBRMaterial } from "@babylonjs/core/Materials/PBR/pbrMaterial";
 import { Color3 } from "@babylonjs/core/Maths/math.color";
-import {
-    AdvancedDynamicTexture,
-    Button,
-    Control,
-    StackPanel,
-    TextBlock,
-} from "@babylonjs/gui";
-import { SceneLoader } from "@babylonjs/core/Loading/sceneLoader";
-import { AbstractMesh } from "@babylonjs/core/Meshes/abstractMesh";
-import { GlowLayer } from "@babylonjs/core/Layers/glowLayer";
-
-// Assets
-import lightbulbModel from "../../assets/glb/incandescent_light_bulb.glb";
-import point from "../../assets/img/point.png";
-import line from "../../assets/img/line.png";
+import { AdvancedDynamicTexture, Button, Control, StackPanel, TextBlock } from "@babylonjs/gui";
+import { CreateBox } from "@babylonjs/core";
 
 export class TestScene implements CreateSceneClass {
     createScene = async (
@@ -37,11 +23,11 @@ export class TestScene implements CreateSceneClass {
         const scene = new Scene(engine);
 
         // This creates and positions a free camera (non-mesh)
-        const cameraRadius = 7;
+        const cameraRadius: number = 15;
         const camera = new ArcRotateCamera(
             "arcRotateCamera",
-            Math.PI / 2,
-            Math.PI / 2,
+            Math.PI/2.5,
+            Math.PI/2.1,
             cameraRadius,
             new Vector3(0, 1, 0),
             scene
@@ -59,99 +45,78 @@ export class TestScene implements CreateSceneClass {
         // This attaches the camera to the canvas
         camera.attachControl(canvas, true);
 
-        // Import 3D model
-        const importResult = await SceneLoader.ImportMeshAsync(
-            "",
-            "",
-            lightbulbModel,
-            scene,
-            undefined,
-            ".glb"
-        );
-
-        // Scale the meshes and isolate the glass bulb (for emission)
-        let bulb: AbstractMesh = new AbstractMesh("bulbPlaceholder");
-        importResult.meshes.forEach((mesh) => {
-            console.log(mesh.name);
-            mesh.scaling.scaleInPlace(4);
-
-            if (mesh.name === "Object_5") bulb = mesh;
-        });
-
-        //Create PBR material
-        const pbr = new PBRMaterial("pbr", scene);
-        pbr.metallic = 0;
-        pbr.roughness = 0;
-        pbr.subSurface.isRefractionEnabled = true;
-        pbr.subSurface.indexOfRefraction = 1.5;
-        pbr.subSurface.tintColor = Color3.White();
-
-        bulb.material = pbr;
-
         /**************************** */
-        const advancedTexture = AdvancedDynamicTexture.CreateFullscreenUI("UI");
+        const advancedTexture = AdvancedDynamicTexture.CreateFullscreenUI('UI');
 
         // INSTRUCTIONS
         const userInstructions = new TextBlock();
-        userInstructions.text = `STAIRCASE SWTICHES
-            Use the switch buttons to control the light
-            The light is ON when both switches have the same shape`;
+        userInstructions.text = 
+            `STACK GEN AND SORT
+            Use the buttons to generate a stack of random cubes
+            and to sort them from the biggest (bottom) to the smallest (top)`;
         userInstructions.color = "white";
         userInstructions.fontSize = 20;
-        userInstructions.top = "30%";
+        userInstructions.top = '30%';
         advancedTexture.addControl(userInstructions);
+        
+        // BUTTONS
+        const generateButton = Button.CreateSimpleButton("generateButton", "GENERATE");
+        const sortButton = Button.CreateSimpleButton("sortButton", "SORT");
 
-        // BUTTON
+        generateButton.horizontalAlignment = Control.VERTICAL_ALIGNMENT_CENTER;
+        generateButton.cornerRadius = 10;
+        generateButton.width = '200px';
+        generateButton.height = '50px';
+        generateButton.color = 'white';
+        generateButton.background = '#AA7777';
+        if(generateButton.textBlock != undefined)
+            generateButton.textBlock.color = 'white';
 
-        const leftSwitch = Button.CreateImageOnlyButton("leftSwitch", point);
-        const rightSwitch = Button.CreateImageOnlyButton("rightSwitch", line);
-
-
-        leftSwitch.horizontalAlignment = Control.VERTICAL_ALIGNMENT_CENTER;
-        leftSwitch.cornerRadius = 10;
-        leftSwitch.width = "50px";
-        leftSwitch.height = "50px";
-        leftSwitch.color = "white";
-        leftSwitch.background = "#AAAAAA";
-        if (leftSwitch.textBlock != undefined)
-            leftSwitch.textBlock.color = "white";
-
-        rightSwitch.horizontalAlignment = Control.VERTICAL_ALIGNMENT_CENTER;
-        rightSwitch.cornerRadius = 10;
-        rightSwitch.width = "50px";
-        rightSwitch.height = "50px";
-        rightSwitch.color = "white";
-        rightSwitch.background = "#AAAAAA";
-        if (rightSwitch.textBlock != undefined)
-            rightSwitch.textBlock.color = "white";
+        sortButton.horizontalAlignment = Control.VERTICAL_ALIGNMENT_CENTER;
+        sortButton.cornerRadius = 10;
+        sortButton.width = '200px';
+        sortButton.height = '50px';
+        sortButton.color = 'white';
+        sortButton.background = '#7777AA';
+        if(sortButton.textBlock != undefined)
+            sortButton.textBlock.color = 'white';
 
         const stackPanel = new StackPanel();
         stackPanel.isVertical = false;
         stackPanel.spacing = 50;
-        stackPanel.top = "10%";
-        stackPanel.addControl(leftSwitch);
-        stackPanel.addControl(rightSwitch);
+        stackPanel.top = '15%';
+        stackPanel.addControl(generateButton);
+        stackPanel.addControl(sortButton);
         stackPanel.zIndex = 1000;
         advancedTexture.addControl(stackPanel);
 
         //TODO: Do something when buttons are pressed
-        leftSwitch.onPointerUpObservable.add(() => {
-            pbr.emissiveColor = Color3.White();
-        });
+        generateButton.onPointerUpObservable.add(()=>{
+            const box = CreateBox('box',
+                {
+                    size: 1,
+                    width: 2,
+                    height: 0.5,
+                }, scene
+            );
 
-        rightSwitch.onPointerUpObservable.add(() => {
-            pbr.emissiveColor = Color3.Black();
+            const box0 = box.createInstance("box0");
+            box0.position = new Vector3(0,1,0);
+            box0.scaling = new Vector3(1.5,1.5,1.5);
         });
-
         /**************************** */
 
         /////////
         // ENV
         /////////
-        //Directional light
+        //Directional light               
         const dlightPosition = new Vector3(0.02, -0.05, -0.05);
         const dLightOrientation = new Vector3(0, 20, 0);
-        const dLight = new DirectionalLight("dLight", dlightPosition, scene);
+        const dLight = new DirectionalLight(
+            "dLight",
+            dlightPosition,
+            scene
+        );
         dLight.intensity = 0.2;
         dLight.position.y = 10;
 
@@ -159,11 +124,7 @@ export class TestScene implements CreateSceneClass {
         dLight.position = dLightOrientation;
 
         // This creates a light, aiming 0,1,0 - to the sky (non-mesh)
-        const hLight = new HemisphericLight(
-            "light",
-            new Vector3(0, 1, 0),
-            scene
-        );
+        const hLight = new HemisphericLight("light", new Vector3(0, 1, 0), scene);
 
         // Default intensity is 1. Let's dim the light a small amount
         hLight.intensity = 0.7;
@@ -171,15 +132,9 @@ export class TestScene implements CreateSceneClass {
         const env = scene.createDefaultEnvironment({
             createSkybox: true,
             skyboxSize: 150,
-            skyboxColor: new Color3(0.01, 0.01, 0.01),
+            skyboxColor: new Color3(0.01,0.01,0.01),
             createGround: false,
         });
-
-        const glow = new GlowLayer("glow", scene, {
-            mainTextureFixedSize: 2048,
-            blurKernelSize: 128,
-        });
-        glow.intensity = 0.5;
 
         return scene;
     };
